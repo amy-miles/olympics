8676563bde470f6e6265c110e636555c76b51661
Amy Miles <33338870+amy-miles@users.noreply.github.com>
2023-10-17 00:41:48 -0500

    added OlympicViewModel

--- CIS174_AmyMiles/Controllers/OlympicController.cs
+++ CIS174_AmyMiles/Controllers/OlympicController.cs
@@ -11,39 +11,54 @@ namespace CIS174_AmyMiles.Controllers
             context = ctx;
         }
 
-        public ViewResult Index(string activeDivision = "all", string activeSport = "all")        
+        public ViewResult Index(OlympicViewModel model)        
         {
-            //store selected division nad sport IDs in view bag
-            ViewBag.ActiveDivision = activeDivision;
-            ViewBag.ActiveSport = activeSport;
+            /**************using ViewModel****************************/
 
-            //store divisions, settings and sports from the db in view bag
-            //ViewBag.Divisions = context.Divisions.ToList();
-            //ViewBag.Sports = context.Sports.ToList();
-            //ViewBag.Settings = context.Settings.ToList();
+            model.Divisions = context.Divisions.ToList();
+            model.Sports = context.Sports.ToList();
 
-            //get list of divisions and sports
-            List<Division> divisions = context.Divisions.ToList();
-            List<Sport> sports = context.Sports.ToList();
-
-            //insert default "All" value at front fo each list
-            divisions.Insert(0, new Division { DivisionID = "all", Name = "All" }) ;
-            sports.Insert(0, new Sport { SportID = "all", Name = "All" });
-
-            //save lists
-            ViewBag.Divisions = divisions ;
-            ViewBag.Sports = sports ;
-
-            //get countries - filter by division and sport
             IQueryable<Country> query = context.Countries.OrderBy(c => c.Name);
-            if (activeDivision != "all") 
-                query = query.Where(c => c.Division.DivisionID.ToLower() == activeDivision.ToLower());
-            if (activeSport != "all")
-                query = query.Where(c => c.Sport.SportID.ToLower() == activeSport.ToLower());
+            if (model.ActiveDivision != "all")
+                query = query.Where(c => c.Division.DivisionID.ToLower() == model.ActiveDivision.ToLower());
+            if (model.ActiveSport != "all")
+                query = query.Where(c => c.Sport.SportID.ToLower() == model.ActiveSport.ToLower());
+            model.Countries = query.ToList();
+            return View(model);
 
-            //pass the list of countries to the view as model
-            var countries = query.ToList();
-            return View(countries);
+
+
+
+
+
+
+            /***********************using viewbag*********************/
+            /** //store selected division nad sport IDs in view bag
+             ViewBag.ActiveDivision = activeDivision;
+             ViewBag.ActiveSport = activeSport;           
+
+             //get list of divisions and sports
+             List<Division> divisions = context.Divisions.ToList();
+             List<Sport> sports = context.Sports.ToList();
+
+             //insert default "All" value at front fo each list
+             divisions.Insert(0, new Division { DivisionID = "all", Name = "All" }) ;
+             sports.Insert(0, new Sport { SportID = "all", Name = "All" });
+
+             //save lists
+             ViewBag.Divisions = divisions ;
+             ViewBag.Sports = sports ;
+
+             //get countries - filter by division and sport
+             IQueryable<Country> query = context.Countries.OrderBy(c => c.Name);
+             if (activeDivision != "all") 
+                 query = query.Where(c => c.Division.DivisionID.ToLower() == activeDivision.ToLower());
+             if (activeSport != "all")
+                 query = query.Where(c => c.Sport.SportID.ToLower() == activeSport.ToLower());
+
+             //pass the list of countries to the view as model
+             var countries = query.ToList();
+             return View(countries);**/
         }
     }
 }
---
+++ CIS174_AmyMiles/Models/OlympicViewModel.cs
@@ -0,0 +1,18 @@
+﻿namespace CIS174_AmyMiles.Models
+{
+    public class OlympicViewModel
+    {
+        public string ActiveDivision { get; set; } = "all";
+        public string ActiveSport { get; set;} = "all";
+
+        public List<Country> Countries { get; set; } = new List<Country>();
+        public List<Division> Divisions { get; set; } = new List<Division>();
+        public List<Sport> Sports { get; set; } = new List<Sport>();
+
+        //methods to help view determine active link
+        public string CheckActiveDivision(string d) => d.ToLower() == ActiveDivision.ToLower() ? "active" : "";
+
+        public string CheckActiveSport(string d) => d.ToLower() == ActiveSport.ToLower() ? "active" : "";
+
+    }
+}
