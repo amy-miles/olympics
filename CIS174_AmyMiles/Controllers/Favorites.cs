1e9552269867c60ee3a034820b2aaf4c32af8747
Amy Miles <33338870+amy-miles@users.noreply.github.com>
2023-10-18 00:28:06 -0500

    1. Added _olympic layout 2. Added Favorites controller 3. Added Details page

---
+++ CIS174_AmyMiles/Controllers/Favorites.cs
@@ -0,0 +1,21 @@
+﻿using CIS174_AmyMiles.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CIS174_AmyMiles.Controllers
+{
+    public class Favorites : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public RedirectToActionResult Add(Country country)
+        {
+            //code to store favorite team in session goes here from ch 9
+            TempData["message"] = $"{country.Name} added to your favorites";
+            return RedirectToAction("Index", "Olympic");//changed from home controller to Olympic
+        }
+    }
+}
--- CIS174_AmyMiles/Controllers/OlympicController.cs
+++ CIS174_AmyMiles/Controllers/OlympicController.cs
@@ -1,5 +1,7 @@
 ﻿using CIS174_AmyMiles.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 
 namespace CIS174_AmyMiles.Controllers
 {
@@ -24,41 +26,29 @@ namespace CIS174_AmyMiles.Controllers
             if (model.ActiveSport != "all")
                 query = query.Where(c => c.Sport.SportID.ToLower() == model.ActiveSport.ToLower());
             model.Countries = query.ToList();
+            ViewBag.Message = TempData["message"];//do I need this?
             return View(model);
 
 
-
-
-
-
-
-            /***********************using viewbag*********************/
-            /** //store selected division nad sport IDs in view bag
-             ViewBag.ActiveDivision = activeDivision;
-             ViewBag.ActiveSport = activeSport;           
-
-             //get list of divisions and sports
-             List<Division> divisions = context.Divisions.ToList();
-             List<Sport> sports = context.Sports.ToList();
-
-             //insert default "All" value at front fo each list
-             divisions.Insert(0, new Division { DivisionID = "all", Name = "All" }) ;
-             sports.Insert(0, new Sport { SportID = "all", Name = "All" });
-
-             //save lists
-             ViewBag.Divisions = divisions ;
-             ViewBag.Sports = sports ;
-
-             //get countries - filter by division and sport
-             IQueryable<Country> query = context.Countries.OrderBy(c => c.Name);
-             if (activeDivision != "all") 
-                 query = query.Where(c => c.Division.DivisionID.ToLower() == activeDivision.ToLower());
-             if (activeSport != "all")
-                 query = query.Where(c => c.Sport.SportID.ToLower() == activeSport.ToLower());
-
-             //pass the list of countries to the view as model
-             var countries = query.ToList();
-             return View(countries);**/
         }
+
+        [HttpPost]//added this from pg 315
+        public RedirectToActionResult Add(Country country)
+        {
+            //code that updates the server with the new team data
+            TempData["message"] = $"{country.Name} added to your favorites";
+            return RedirectToAction("Index", "Olympic");
+        }
+
+       public IActionResult Details(string id)//page 323
+        {
+            var country = context.Countries
+                .Include(c => c.Division)
+                .Include(c => c.Sport)
+                .FirstOrDefault(c => c.CountryID == id) ?? new Country();
+            return View(country);
+        }
+
+
     }
 }
